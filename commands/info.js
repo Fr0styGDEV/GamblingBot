fdbe653e72d9a3388623ff0d9e756a05f4f19a09
Fr0styGDEV <nathanieltrotter04@gmail.com>
2024-12-19 12:18:39 -0600

    updated !info to include new commands

--- commands/info.js
+++ commands/info.js
@@ -14,12 +14,13 @@ module.exports = {
                 { name: '!rob @user <amount> / !r @user <amount>', value: 'Attempt to rob coins from another player (50/50 chance).', inline: true },
                 { name: '!give @user <amount>', value: 'Give coins to another player.', inline: true },
                 { name: '!jackpot <amount> <odds>/ !jp <amount> <odds>', value: 'You set your reward multiplier, but beware—its also your odds!', inline: true },
-                { name: '!leaderboard / !lb', value: 'View the top 5 players by coin balance.', inline: true },
+                { name: '!leaderboard / !lb', value: 'View the top 5 players by coin balance and levels.', inline: true },
                 { name: '!frostycoin / !fc', value: 'View the current price and history of FrostyCoin®💎 (updates every minute)', inline: true },
-                { name: '!buy', value: 'Purchase FrostyCoin®💎 at the current market value', inline: true },
-                { name: '!sell', value: 'Sell your FrostyCoin®💎 at the current market value ', inline: true },
+                { name: '!buy', value: 'Purchase FrostyCoin®💎 at the current market value.', inline: true },
+                { name: '!sell', value: 'Sell your FrostyCoin®💎 at the current market value.', inline: true },
                 { name: '!earn', value: 'Answer gaming related trivia questions for coins!', inline: true },
-                { name: '!info / !commands', value: 'Display this help message with all commands.', inline: true },
+                { name: '!levelup <levels>', value: 'Spend coins to level up and increase your rank.', inline: true },
+                { name: '!info / !commands', value: 'Display this help message with all commands.', inline: true }
             )
             .setFooter({ text: 'GamblingBOT® - Have fun and gamble responsibly! 😉', }) // Replace with an icon URL if desired
             .setTimestamp();
